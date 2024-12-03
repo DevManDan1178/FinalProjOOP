7ea0b0422d0237547d55cfc2a6cfa44a1bc73392
Name <l3r0d4niel@gmail.com>
2024-12-03 16:42:42 -0500

    ye
    a

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        //test books
         Book b = new Book("\"Sigma box fights ruined my life\" - how elon Musk brought back OG fortnite");
         Book b2 = new Book("how jeff Bezos survived the fortnite storm");
         Book b3 = new Book("why the world feared EDP's perfected recipe for homemade cupcakes");
